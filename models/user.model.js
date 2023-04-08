439a9ff80344a7be59ad77fd4dd2a728e5f84628
sri-rishi <sririshi2612@gmail.com>
2023-04-08 16:38:19 +0530

    feat: created model for users

---
+++ models/user.model.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const UserSchema = mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: true
+        },
+        password: {
+            type: String,
+            required: true
+        },
+        firstname: {
+            type: String,
+            required: true
+        },
+        lastname: {
+            type: String,
+            required: true
+        },
+        profilePicture: String,
+        coverPicture: String,
+        about: String,
+        livesIn: String,
+        worksAt: String,
+        followers: [],
+        following: [],
+    },
+    {
+        timestamps: true
+    }
+)
+
+const UserModel = mongoose.model("User", UserSchema);
+
+module.exports = UserModel;
