f2bb6dba25e53c5d6d83554dd6cbb1f3e81485be
sri-rishi <sririshi2612@gmail.com>
2023-04-08 17:13:13 +0530

    chore: updated dependency

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
 const cors = require("cors");
 const connectDB = require("./db");
 const auth = require("./routes/auth.routes");
